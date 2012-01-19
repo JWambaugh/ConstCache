ea10eb8ff973e9ea86e38210561ef3c2963cf1d7
Jordan Wambaugh <jordan@wambaugh.org>
2012-01-18 17:35:54 -0800

    changed test a bit

--- test.php
+++ test.php
@@ -1,12 +1,30 @@
 <?php
-dl('constcache.so');
+//dl('constcache.so');
 //var_dump(get_loaded_extensions());
 $cache=new ConstCache("phpTest.cache",1200);
 if($cache->isWritable()){
 	$cache->prepareForWrite();
 	$cache->set("myKey","My value!!!!");
+	$cache->set("myotherKey",array("This is a complex type","Because it is an array."));
 	$cache->finalize();
 }
-$key= $cache->get("myKey");
-var_dump($key);
+$cache2=new ConstCache("phpTest.cache",1200);
+
+
+$val= $cache->get("myKey");
+var_dump($val);
+echo "here";
+
+$val= $cache2->get("myKey");
+var_dump($val);
+
+echo "not here";
+$val ="BLAH";
+
+$val= $cache2->get("myKey");
+var_dump($val);
+
+
+
+
 ?>
